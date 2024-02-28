5cc262a53d6d048d01e93db5abb75ffc21b78603
Swapnil Jha <swapnil.strange0707@gmail.com>
2024-02-28 12:50:25 +0530

    notification_code_hidden

--- index2.js
+++ index2.js
@@ -1,7 +1,9 @@
 const btn1 = document.querySelector('#btn1')
 const btn2 = document.querySelector('#btn2')
+const btn3 = document.querySelector('#btn3')
 const idea = document.querySelector('#idea')
 const quiz = document.querySelector('#quiz')
+const notifications = document.querySelector('#notif')
 
 if (btn1) {
     btn1.addEventListener('click', (e) => {
@@ -20,7 +22,6 @@ if (btn1) {
 if (btn2) {
     btn2.addEventListener('click', (e) => {
         e.preventDefault()
-        console.log("Hello");
         const link = document.createElement("a");
         const content = quiz.textContent
         const file = new Blob([content], { type: 'text/plain' });
@@ -30,5 +31,20 @@ if (btn2) {
         URL.revokeObjectURL(link.href);
     })
 }
+
+
+if (btn3) {
+    btn3.addEventListener('click', (e) => {
+        e.preventDefault()
+        const link = document.createElement("a");
+        const content = notifications.textContent
+        const file = new Blob([content], { type: 'text/plain' });
+        link.href = URL.createObjectURL(file);
+        link.download = "notif.txt";
+        link.click();
+        URL.revokeObjectURL(link.href);
+    })
+}
+
 
 
